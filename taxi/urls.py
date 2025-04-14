890bb36cac8463c2bc60d566a0ce117aa5a0a957
Maksym Omelkov <omelkov85@gmail.com>
2025-04-14 22:16:59 +0300

    Taxi service user form and validation (1.1)

--- taxi/urls.py
+++ taxi/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path(
-        "cars/<int:pk>/add-me",
+        "cars/<int:pk>/add-me/",
         CarAddCurrentDriverView.as_view(),
         name="car-driver-add-current"
     ),
